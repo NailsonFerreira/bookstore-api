d1b538d3925539a6544b620d6a4bb318f28c1ff7
Nailson Ferreira <nailson_ferreira@hotmail.com>
2021-06-27 22:04:53 -0300

    Add Domain Classes

---
+++ src/main/java/br/com/nailson/domain/Categoria.java
@@ -0,0 +1,17 @@
+package br.com.nailson.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Categoria {
+	
+	private Integer Id;
+	private String nome;
+	private String descricao;
+
+	private List<Livro> livros = new ArrayList<Livro>();
+	
+	
+	
+	
+}
---
+++ src/main/java/br/com/nailson/domain/Livro.java
@@ -0,0 +1,11 @@
+package br.com.nailson.domain;
+
+public class Livro {
+	
+	private Integer Id;
+	private String titulo;
+	private String nomeAutor;
+	private String texto;
+	private Categoria categoria;
+	
+}
